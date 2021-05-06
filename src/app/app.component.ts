0517cfc2d9bcf3e9934c504136888be9b0dd6c66
Alison Orellana <ali15.orellan@gmail.com>
2021-05-06 10:26:34 -0400

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //Nombre de la etiqueta html de ese componente
   selector: 'app-root',
+  //donde esta el html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-app-angular';
+  title = 'mi-primera-app-angular';
 }
